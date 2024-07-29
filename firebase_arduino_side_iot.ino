9675249f4e4ee79ee42c0b7c8e2ca9843b5ab92b
Inzar Salfikar <inzar86@users.noreply.github.com>
2024-07-29 16:09:31 +0700

    Add files via upload

---
+++ firebase_arduino_side_iot.ino
@@ -0,0 +1,32 @@
+#include <MQ7.h>
+
+// here Iam using MQ7 as example (you can use other sensor or dummy data like random)
+MQ7 mq7(A0, 5.0);
+
+float data1;
+int data2;
+int data3
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  //  // put your main code here, to run repeatedly:
+  data1 = mq7.getPPM();    //real data from sensor MQ7
+  data2 = random(0, 100);  // example random data tobe send to ESP over serial
+  data3 = random(200, 300);
+
+  Serial.print(api);
+  Serial.print(",");
+  Serial.print(tekanan);
+  Serial.print(",");
+  Serial.print(carbon);
+  Serial.print(",");
+  Serial.println("X");
+
+  // give at leat 1 second delay for next sending.
+  delay(max_delay);
+}
---
+++ firebase_esp8266_side_iot.ino
@@ -0,0 +1,73 @@
+#include "FirebaseESP8266.h"  // Install Firebase ESP8266 library
+#include <ESP8266WiFi.h>
+
+#define FIREBASE_HOST "Realtime database link here example:project1234-default-rtdb.firebaseio.com/"
+#define FIREBASE_AUTH "Realtime database secret code here example:cKGpr86lq5sLRrEdW6n9djsfhjgdsdhabjjabhbcaj"
+#define WIFI_SSID "YourSSID or Accespoint"
+#define WIFI_PASSWORD "yourWifiPassword"
+
+//Define FirebaseESP8266 data object
+FirebaseData Data;
+
+FirebaseJson json;
+
+String simpandata = "";
+String perintah;
+
+int sensor1, sensor2, sensor3;
+
+String inputString = "";         // a String to hold incoming data
+boolean stringComplete = false;  // whether the string is complet
+
+int data[3];
+int urutan;
+
+void setup() {
+  Serial.begin(9600);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(300);
+  }
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+}
+
+void loop() {
+
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    if (inChar == ',') {
+      data[urutan] = inputString.toFloat();
+      inputString = "";
+      urutan++;
+      inChar = 0;
+      goto jump;
+    }
+    // add it to the inputString:
+    inputString += inChar;
+jump:
+
+    if (inChar == 'X') {
+      for (int i = 0; i < 3; i++) {
+        Serial.print(data[i]);
+        Serial.print(",");
+      }
+
+      // clear the string:
+      urutan = 0;
+      inputString = "";
+
+      // For running
+      sensor1 = data[0];
+      sensor2 = data[1];
+      sensor3 = data[2];
+
+      Firebase.setString(Data, "/yourFirebaseProjectBucket/yourFirebaseVariable1", (String)sensor1);
+      Firebase.setString(Data, "/yourFirebaseProjectBucket/yourFirebaseVariable2", (String)sensor2);
+      Firebase.setString(Data, "/yourFirebaseProjectBucket/yourFirebaseVariable3", (String)sensor3);
+      delay(1000);
+    }
+  }
+}
